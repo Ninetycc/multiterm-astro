7c0f4d1d4b1d56d7ebfe3940cb8c357592fccfa6
Ninetycc <pe@ipgo.me>
2025-11-01 15:30:42 +1100

    Enoch

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,9 +2,9 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://multiterm.stelclementine.com',
+  site: 'https://1934.date',
   // The name of your site, used in the title and for SEO.
-  title: 'MultiTerm',
+  title: '1934.date',
   // The description of your site, used for SEO and RSS feed.
   description:
     'A coder-ready Astro blog theme with 59 of your favorite color schemes to choose from',
